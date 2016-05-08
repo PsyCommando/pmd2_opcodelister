165b54e7e07794e3f53cb6fc754cce971895b378
psy_commando <psycommando@gmail.com>
2016-05-08 15:40:33 -0400

    Updated for supporting dumping both EoT and EoS
    It needs overlay_0011.bin and overlay_0004.bin from EoS and EoT, in the
    same folder, respectively, to do its job.

--- main.cpp
+++ main.cpp
@@ -71,15 +71,25 @@ template<class T, class _init>
 }
 
 /************************************************************************************
-    opcodetblentry
+    opcodetblentry_eos
         Contain opcode data from the opcode table.
 ************************************************************************************/
-struct alignas(8) opcodetblentry
+struct alignas(8) opcodetblentry_eos
 {
     int8_t   nbparams;
     int8_t   unk1;
     int8_t   unk2;
     int8_t   unk3;
+    uint32_t stringoffset;
+};
+
+/************************************************************************************
+    opcodetblentry_eot
+        Contain opcode data from the opcode table.
+************************************************************************************/
+struct opcodetblentry_eot
+{
+    int8_t   nbparams;
     uint32_t stringoffset;
 };
 
@@ -114,23 +124,41 @@ std::vector<uint8_t> LoadFile( const std::string & fpath )
 }
 
 /************************************************************************************
-    main
-        This is where all the work is done.
+    FetchString
+        Fetchs a null terminated C-String from a file offset.
 ************************************************************************************/
-int main( int argc, const char * argv[] )
+template<typename _init>
+    std::string FetchString( uint32_t fileoffset, _init itfbeg, _init itfend )
+{
+    auto    itstr = itfbeg;
+    std::advance( itstr,  fileoffset );
+    size_t  strlength = safestrlen(itstr, itfend);
+    string  dest;
+    dest.resize(strlength);
+
+    for( size_t cntchar = 0; cntchar < strlength; ++cntchar, ++itstr )
+        dest[cntchar] = (*itstr);
+
+    return std::move(dest);
+}
+
+/************************************************************************************
+    DumpEoS
+        Dumps the opcode table and nb of parameters, and etc, for Explorers of Sky!
+************************************************************************************/
+void DumpEoS( const string & overlayfname, const string & outfname )
 {
     static const size_t         NBOpcodes   = 383;       //Nb of entries in the opcode table.
     static const uint32_t       OffsetTable = 0x3C3D0;   //Offset of the opcode table in the overlay_0011 file for EoS. 
     static const uint32_t       DiffPointer = 0x22DC240; //Difference between memory space and overlay file space for pointer addresses
-    string                      overlayfname= "overlay_0011.bin";
-    string                      outfname    = "opcodelist.txt";
+
     vector<uint8_t>             fdat( LoadFile(overlayfname) );
     auto                        itbeg = fdat.begin();
     auto                        itend = fdat.end();
-    vector<opcodetblentry>      entries(NBOpcodes, opcodetblentry{0,0,0,0,0} );
+    vector<opcodetblentry_eos>  entries(NBOpcodes, opcodetblentry_eos{0,0,0,0,0} );
     vector<string>              opnames(NBOpcodes);
 
-    cout<<"Started!\n";
+    cout<<"Started EoS!\n";
     //Advance iterator to beginning of opcode table
     std::advance(itbeg, OffsetTable);
     
@@ -151,13 +179,7 @@ int main( int argc, const char * argv[] )
         
         //Scoop up the string from the string table
         uint32_t  straddr = entry.stringoffset - DiffPointer;
-        auto itstr = fdat.begin() + straddr;
-
-        size_t strlength = safestrlen(itstr, itend);
-        opnames[i].resize(strlength);
-
-        for( size_t cntchar = 0; cntchar < strlength; ++cntchar, ++itstr )
-            opnames[i][cntchar] = (*itstr);
+        opnames[i]        = std::move( FetchString(straddr, fdat.begin(), itend) );
 
         cout <<"\rRead " <<setfill(' ') <<setw(3) <<left <<i + 1 ;
     }
@@ -165,8 +187,9 @@ int main( int argc, const char * argv[] )
     //### Phase 2: Dump everything to a text file ###
     //* I could have done this directly in the first loop, but its probably better to keep those separated 
     //  in case I want to re-use this.
-    cout<<"\nWriting!\n";
-    ofstream of(outfname);
+    cout<<"\nWriting EoS!\n";
+    ofstream            of(outfname);
+    static const size_t LongestString = 35; //Calculate that dynamically one day maybe
 
     of << "=============================================================\n"
        << "\tScript OpCode List\n"
@@ -177,7 +200,7 @@ int main( int argc, const char * argv[] )
     {
         stringstream sstr;
         sstr <<"\t0x" <<hex <<setfill('0') <<setw(3) <<right <<i <<dec 
-            <<" - " <<setfill(' ') <<setw(35) <<left <<opnames[i] <<", " 
+            <<" - " <<setfill(' ') <<setw(LongestString) <<left <<opnames[i] <<", " 
             <<setfill(' ') <<setw(2) <<right
             <<static_cast<short>(entries[i].nbparams)
             <<" params, Unk1: " <<setfill(' ') <<setw(3) <<right  <<static_cast<short>(entries[i].unk1) 
@@ -187,6 +210,80 @@ int main( int argc, const char * argv[] )
     }
 
     cout <<"\nDone!\n";
+}
 
+/************************************************************************************
+    DumpEoT
+        Dumps the opcode table and nb of parameters for Explorers of Time!
+************************************************************************************/
+void DumpEoT( const string & overlayfname, const string & outfname )
+{
+    static const size_t         NBOpcodes            = 336;       //Nb of entries in the opcode table.
+    static const uint32_t       OffsetStringPtrTable = 0x5CAAC;   //
+    static const uint32_t       OffsetParamTable     = 0x509C0;   //Offset in the overlay of the table containing the nb of params for each command
+    static const uint32_t       OverlayLoadOffset    = 0x22D5B20;
+
+    vector<uint8_t>             fdat( LoadFile(overlayfname) );
+    auto                        itbeg = fdat.begin();
+    auto                        itend = fdat.end();
+    vector<opcodetblentry_eot>  entries(NBOpcodes, opcodetblentry_eot{0,0} );
+    vector<string>              opnames(NBOpcodes);
+
+    cout<<"Started EoT!\n";
+    //Advance iterator to beginning of string ptr table
+    auto itbegptr = itbeg;
+    std::advance(itbegptr, OffsetStringPtrTable);
+    //Place an iterator to the start of the parameter nb table
+    auto itbegparams = itbeg;
+    std::advance(itbegparams, OffsetParamTable);
+
+    //### Phase 1: Load up the data ###
+    for( size_t i = 0; i < NBOpcodes; ++i )
+    {
+        //Get the data
+        entries[i].stringoffset = ReadIntFromBytes<uint32_t>( itbegptr, itend );    //iterator incremented
+        entries[i].nbparams     = ReadIntFromBytes<int8_t>  ( itbegparams, itend ); //iterator incremented
+
+        //Get the string
+        uint32_t stroffset = entries[i].stringoffset - OverlayLoadOffset;
+        opnames[i]         = std::move( FetchString(stroffset, fdat.begin(), itend) );
+
+        cout <<"\rRead " <<setfill(' ') <<setw(3) <<left <<i + 1 ;
+    }
+
+    //### Phase 2: Dump everything to a text file ###
+    //* I could have done this directly in the first loop, but its probably better to keep those separated 
+    //  in case I want to re-use this.
+    cout<<"\nWriting EoT!\n";
+    ofstream of(outfname);
+    static const size_t LongestString = 35; //Calculate that dynamically one day maybe
+
+    of << "=============================================================\n"
+       << "\tScript OpCode List for EoT\n"
+       << "=============================================================\n";
+
+    //Dump to output
+    for( size_t i = 0; i < NBOpcodes; ++i )
+    {
+        stringstream sstr;
+        sstr <<"\t0x" <<hex <<setfill('0') <<setw(3) <<right <<i <<dec <<" - " 
+            <<setfill(' ') <<setw(LongestString) <<left <<opnames[i] <<", " 
+            <<setfill(' ') <<setw(2) <<right
+            <<static_cast<short>(entries[i].nbparams) <<"\n";
+        of << sstr.str();
+    }
+
+    cout <<"\nDone!\n";
+
+}
+
+/************************************************************************************
+    main
+        This is where all the work is done.
+************************************************************************************/
+int main( int argc, const char * argv[] )
+{
+    DumpEoS( "overlay_0011.bin", "opcodelist_eos.txt" );
+    DumpEoT( "overlay_0004.bin", "opcodelist_eot.txt" );
     return 0;
 }
